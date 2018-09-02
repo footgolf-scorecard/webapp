bc301661a51a2163a98c12bb3668faaa3f1a3d84
Michael Berber <mickberber@icloud.com>
2018-09-01 23:18:18 -0700

    add react, add to linter

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return (
+      <div>hey fg scorecard</div>
+    );
+  }
+};
+
+export default App;

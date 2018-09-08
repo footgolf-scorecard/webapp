33936dec05979e6ae7089597ad2725fb32fd52b4
Thefutureseer <30768475+thefutureseer@users.noreply.github.com>
2018-09-08 11:30:38 -0700

    first files title left, login right on nav

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import './../styles/index.css';
 import NavBar from './navBar';
 
+
+
 class App extends Component {
   render() {
     return (
--- src/components/navBar.js
+++ src/components/navBar.js
@@ -1,15 +1,24 @@
 import React, { Component } from 'react';
 
+
 class NavBar extends Component {
   handleClick() {}
 
   render() {
     return (
-      <div className="navWide">
-        <div className="fgHead">Test1</div>
-        <div className="fgButton">
-          <button onClick={this.handleClick}>Login</button>
+      <div class = "navBody">
+      <div className="navWide" class="row1">
+        <div className="wideTop">
+        <div class="column1">
+         <div className="fgHead">Footgolf Scorecard</div>
         </div>
+        <div class="column2">
+        <div className="fgButton">
+         <button onClick={this.handleClick}>Login</button>
+        </div>
+        </div>
+        </div>
+      </div>
       </div>
     );
   }

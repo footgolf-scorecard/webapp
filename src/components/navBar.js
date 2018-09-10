b3cde7a96c3ef6db9c186b60f15d803e3e19638b
Thefutureseer <30768475+thefutureseer@users.noreply.github.com>
2018-09-10 14:48:58 -0700

    style, override default button

--- src/components/navBar.js
+++ src/components/navBar.js
@@ -6,23 +6,24 @@ class NavBar extends Component {
 
   render() {
     return (
-      <div class = "navBody">
-      <div className="navWide" class="row1">
-        <div className="wideTop">
-        <div class="column1">
-         <div className="fgHead">Footgolf Scorecard</div>
+      <div className= "navBody">
+        <div className="navWide" className="row1">
+          <div className="wideTop">
+            <div className="column1">
+             <div className="fgHead">Footgolf Scorecard</div>
+            </div>
+            <div className="column2">
+              <div className='fgButton'>
+               <button>Login</button>
+              </div>
+            </div>
+          </div>
         </div>
-        <div class="column2">
-        <div className="fgButton">
-         <button onClick={this.handleClick}>Login</button>
-        </div>
-        </div>
-        </div>
-      </div>
       </div>
     );
   }
 };
+
 
 
 export default NavBar;
